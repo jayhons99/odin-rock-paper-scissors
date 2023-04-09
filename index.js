6bff489b1ba4d9362ca690ff4d9b64b3e83315da
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-04-09 13:29:49 -0400

    built a working skeleton of ui

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+let COMPUTER_WINS = 0;
+let PLAYER_WINS = 0;
+
 function getComputerChoice() {
   let choice = ["rock", "paper", "scissors"];
   let random = Math.floor(Math.random() * choice.length);
   return choice[random];
 }
 
-// console.log(getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
   let result = "";
+  let didPlayerWin = false;
   if (playerSelection === computerSelection) {
+    checkWinner();
     return "You both play " + playerSelection + ". Draw!";
   }
   switch (playerSelection.toLowerCase()) {
     case "rock":
       if (computerSelection === "scissors") {
         result = "You win!";
+        didPlayerWin = true;
       } else {
         result = "You lose!";
       }
@@ -22,6 +26,7 @@ function playRound(playerSelection, computerSelection) {
     case "scissors":
       if (computerSelection === "paper") {
         result = "You win!";
+        didPlayerWin = true;
       } else {
         result = "You lose!";
       }
@@ -29,11 +34,16 @@ function playRound(playerSelection, computerSelection) {
     case "paper":
       if (computerSelection === "rock") {
         result = "You win!";
+        didPlayerWin = true;
       } else {
         result = "You lose!";
       }
       break;
   }
+
+  updateScore(didPlayerWin);
+  checkWinner();
+
   return (
     "The opponent plays " +
     computerSelection +
@@ -44,16 +54,54 @@ function playRound(playerSelection, computerSelection) {
   );
 }
 
-// console.log(playRound("rock", getComputerChoice()));
+function updateScore(didPlayerWin) {
+  const compScore = document.querySelector("#compScore");
+  const playerScore = document.querySelector("#playerScore");
+
+  if (didPlayerWin) {
+    PLAYER_WINS++;
+  } else {
+    COMPUTER_WINS++;
+  }
+
+  compScore.textContent = COMPUTER_WINS;
+  playerScore.textContent = PLAYER_WINS;
+}
+
+function checkWinner() {
+  const outcome = document.querySelector("#isGameOver");
+  // if (PLAYER_WINS === 5 || COMPUTER_WINS === 5) {
+  //   outcome.textContent = "Game Over!";
+  //   PLAYER_WINS = 0;
+  //   COMPUTER_WINS = 0;
+  // } else {
+  //   outcome.textContent = "Keep playing!";
+  // }
+
+  if (PLAYER_WINS === 5) {
+    outcome.textContent = "You have won, congratulations!";
+    PLAYER_WINS = 0;
+    COMPUTER_WINS = 0;
+  } else if (COMPUTER_WINS === 5) {
+    outcome.textContent = "You have lost. Try again.";
+    PLAYER_WINS = 0;
+    COMPUTER_WINS = 0;
+  } else {
+    outcome.textContent = "First to win 5 rounds wins the game!";
+  }
+}
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt(
-      "Let's play rock, paper, scissors! What do you play?"
-    );
-    let normalizedChoice = playerChoice.toLowerCase();
-    console.log(playRound(normalizedChoice, getComputerChoice()));
-  }
+  const buttons = document.querySelectorAll(".choice");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const display = document.querySelector("#result");
+      const compChoice = getComputerChoice();
+      const outcome = playRound(btn.textContent.toLowerCase(), compChoice);
+      display.textContent = outcome;
+    });
+  });
 }
 
 game();
